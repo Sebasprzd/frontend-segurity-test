8c9ab43521bec517ac5b3d27e84c2028ae766108
Sebasprzd <seperazade@est.utn.ac.cr>
2025-07-13 23:40:49 -0600

    Proyecto Base

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Sign Up</h1>
+      {/* El formulario de registro irá aquí */}
+    </div>
+  );
+};
+
+export default SignUp;
